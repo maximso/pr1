e72df0b97768c18bcf5b0aac545033df333402e7
maximso <70947590+maximso@users.noreply.github.com>
2020-09-08 15:21:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using System;
+struct pract
+{
+  static void Main(string[] args)
+  {
+    string s = "Microsoft .NET Framework 2 Application Development Foundation";
+    string[] sa = s.Split(" ");
+    Array.Sort(sa);
+    s = string.Join(" ", sa);
+    Console.WriteLine(s);
+  }
+}
